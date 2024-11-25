f640b4a4b9ac5a1849395fbb563943503282d843
Theo Spencer <theo.spencer@consultspencer.com>
2024-11-25 09:42:27 -0500

    Updated version

--- starships/src/app/app.component.ts
+++ starships/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {DatePipe, NgFor} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { GetShipsService } from './services/api/getships.service';
-import { HttpClientModule } from "@angular/common/http";
+import {} from "@angular/common/http";
 import {starship} from "./interfaces/StarshipResponse";
 import { MatExpansionModule } from '@angular/material/expansion'
 import { MatButtonToggleModule } from '@angular/material/button-toggle'
@@ -13,7 +13,11 @@ import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, NgFor, MatExpansionModule, DatePipe, MatButtonToggleModule, MatButtonModule, MatCardModule],
+  imports: [RouterOutlet, 
+// TODO: `HttpClientModule` should not be imported into a component directly.
+// Please refactor the code to add `provideHttpClient()` call to the provider list in the
+// application bootstrap logic and remove the `HttpClientModule` import from this component.
+HttpClientModule, NgFor, MatExpansionModule, DatePipe, MatButtonToggleModule, MatButtonModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [GetShipsService]
